a5291d18bbb4362a390171e6e4994bb0312559c8
Stuuu <stuartschrick@gmail.com>
2021-12-01 22:24:40 -0500

    day 1 | part 2

--- 01/day_1.php
+++ 01/day_1.php
@@ -9,11 +9,17 @@ class DepthDeterminer
         $depth_measures = file('puzzle_inputs.txt', FILE_IGNORE_NEW_LINES);
         $increase_count = 0;
         foreach ($depth_measures as $key => $measure) {
-            // the first measure doesn't have a prior reading
+            // the first 3 measures don't have a prior reading to compare against
             // so it can be ignored
-            if ($key === 0) continue;
+            if ($key <= 2) continue;
 
-            if ($measure > $depth_measures[$key - 1]) {
+
+            $prior_window_sum = $depth_measures[$key - 1] + $depth_measures[$key - 2] + $depth_measures[$key - 3];
+
+            $current_window_sum = $depth_measures[$key] + $depth_measures[$key - 1] +
+                $depth_measures[$key - 2];
+
+            if ($current_window_sum > $prior_window_sum) {
                 $increase_count++;
             }
         }
